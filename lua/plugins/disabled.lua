69a4f7a4840335d03ed32a2a4389ef6ec9553b00
vsdmars <vsdmars@gmail.com>
2023-03-07 08:01:06 -0800

    init

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = true },
   { "goolord/alpha-nvim", enabled = true },
+  -- { "folke/tokyonight.nvim", enabled = false },
 }
---
+++ lua/plugins/white_space.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "ntpeters/vim-better-whitespace",
+    init = function()
+      vim.g.better_whitespace_enabled = 1
+      vim.g.strip_whitespace_on_save = 1
+      vim.g.strip_whitespace_confirm = 0
+      vim.g.strip_whitelines_at_eof = 1
+    end,
+  },
+}
