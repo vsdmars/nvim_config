e40b815267b7e0c77ea38afe03b0fd6b34a1b4f0
vsdmars <vsdmars@gmail.com>
2023-03-10 05:11:29 -0800

    override mason config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "pyright" },
+        automatic_installation = true,
+      })
+    end,
+  },
+}
